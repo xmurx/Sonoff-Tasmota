be80bbbac4104ff0ecc95e3f027b162946e4580c
xmurx <49923554+xmurx@users.noreply.github.com>
2019-07-23 21:30:01 +0200

    deactivate virtual distance measurement

--- sonoff/my_user_config.h
+++ sonoff/my_user_config.h
@@ -482,7 +482,9 @@
 #define USE_SM16716                              // Add support for SM16716 RGB LED controller (+0k7 code)
 
 //#define USE_HRE                                  // Add support for Badger HR-E Water Meter (+1k4 code)
-#define USE_VIRTUAL_DISTANCE_MEASUREMENT           // Add support for virtual distance measurement
+//#define USE_VIRTUAL_DISTANCE_MEASUREMENT           // Add support for virtual distance measurement
+
+#define USE_VL53L0X                               // Add support for VL53L0X
 
 /*********************************************************************************************\
  * Debug features are only supported in development branch
