b4d3ea436a1bccde2b6deb15b7b30031cc313a35
xmurx <49923554+xmurx@users.noreply.github.com>
2020-02-08 23:13:38 +0100

    change due to merge with upstream development branch

--- tasmota/xsns_support.ino
+++ tasmota/xsns_support.ino
@@ -3,7 +3,6 @@
 #include <stdint.h>
 
 #include <Arduino.h>
-#include "sonoff.h"
 #include "logging.h"
 
 /*********************************************************************************************\
