a4e466bee66058ce7b5aafb0d8f530d6b30f3bee
xmurx <ronny.mueller@wom.group>
2020-02-10 09:49:27 +0100

    added unit tests for class StateControl

--- lib/xControl-0.0.1/tst/src/StateControl.cpp
+++ lib/xControl-0.0.1/tst/src/StateControl.cpp
@@ -11,8 +11,24 @@ using ::testing::Eq;
 namespace xControl
 {
   TEST_F(TasmotaFixture, ChangeStateAfterTimeOut)
-  {  
-    
-    EXPECT_TRUE(true);
+  { 
+    EXPECT_CALL(*TasmotaFixture::_time, Millis()).Times(2).WillOnce(Return(0)).WillOnce(Return(2001));
+    StateControl<State> stateControl;
+    State initialState = stateControl.GetState();
+    stateControl.SetState(State::Delay, 2000);
+    bool delayExpired = stateControl.DelayExpired();
+    EXPECT_TRUE((initialState == State::Unknown) && delayExpired );
+  }
+
+  TEST_F(TasmotaFixture, ChangeStateAfterTimeOutAndCallOnEnterOnce)
+  { 
+    EXPECT_CALL(*TasmotaFixture::_time, Millis()).Times(2).WillOnce(Return(0)).WillOnce(Return(2001));
+    StateControl<State> stateControl;
+    State initialState = stateControl.GetState();
+
+    stateControl.SetState(State::Delay, 2000);
+    bool onEnter = stateControl.OnEnter();
+    onEnter = onEnter && stateControl.OnEnter();
+    EXPECT_TRUE((initialState == State::Unknown) && !onEnter );
   }
 } // end of namespace
